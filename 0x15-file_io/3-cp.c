5d302d790a1939c2bb8e93ed089cc648de7e3379
Michael <addictedchrysler@gmail.com>
2023-08-07 12:25:41 -0700

    Some changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,10 +6,10 @@ char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * create_buffer - Gives 1024 bytes for a buffer.
+ * @file: Name of file buffer is storing chars.
  *
- * Return: A pointer to the newly-allocated buffer.
+ * Return: Pointer to the created space of  buffer.
  */
 char *create_buffer(char *file)
 {
@@ -28,8 +28,8 @@ char *create_buffer(char *file)
 }
 
 /**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
+ * close_file - This file close file descriptors.
+ * @fd: file descriptor.
  */
 void close_file(int fd)
 {
@@ -45,16 +45,16 @@ void close_file(int fd)
 }
 
 /**
- * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * main - This copies one file to another file.
+ * @argc: Number of arguments supplied to the program.
+ * @argv: This is an array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
- * Description: If the argument count is incorrect - exit code 97.
- *              If file_from does not exist or cannot be read - exit code 98.
- *              If file_to cannot be created or written to - exit code 99.
- *              If file_to or file_from cannot be closed - exit code 100.
+ * Description: If the argument count is wrong - exit code 97.
+ *              If the file_from cannot be read - exit code 98.
+ *              If the file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from can't be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
